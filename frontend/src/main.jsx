f3fec0344c5ab62d5760609f355cececbb81c31c
vidhisoni1 <vidhisoni8869@gmail.com>
2025-01-18 00:39:33 +0530

    home page

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -1,10 +1,22 @@
-import { StrictMode } from 'react'
+import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import { BrowserRouter } from 'react-router-dom';
+// import UserContext from './context/UserContext.jsx';
+// import CaptainContext from './context/CapatainContext.jsx';
+// import SocketProvider from './context/SocketContext.jsx';
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
+
+ 
+   
+      <StrictMode>
+        <BrowserRouter>
+          <App />
+        </BrowserRouter>
+      </StrictMode>
+    
+ 
+
 )
